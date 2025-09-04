da6b788efeee3d2b5803883af063c7494e019a28
HabibovU <habibovulugbek22@gmail.com>
2025-09-04 18:08:59 +0500

    fix

--- call-center-backend/server.js
+++ call-center-backend/server.js
@@ -91,8 +91,8 @@ left join siebel.s_contact scC on scC.Row_Id=sea.target_per_id
 
 where sea.todo_cd='Call Outbound Communication'
   and sp.postn_type_cd='Софт специалист'
-  and sea.created = TO_DATE(:date, 'YYYY-MM-DD')`,
-      [date]
+  and sea.created = :date`,
+      { date }
     );
 
     const countPerson = [
